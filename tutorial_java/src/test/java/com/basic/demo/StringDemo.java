035907e63b826b8e42c39547ffb5d9c1c0723012
zhoucheng <574693497@qq.com>
2018-08-09 21:52:28 +0800

    java基础

---
+++ tutorial_java/src/test/java/com/basic/demo/StringDemo.java
@@ -0,0 +1,19 @@
+package com.basic.demo;
+
+import org.junit.Test;
+
+/**
+ * @author jimmy
+ * @date 2018/8/921:21
+ */
+public class StringDemo {
+
+    @Test
+    public void StringTest(){
+
+        String str = new String("Hello");
+        System.out.println(str);//hello
+        str = new String("world");
+        System.out.println(str);//world
+    }
+}
