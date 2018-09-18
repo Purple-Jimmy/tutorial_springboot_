78bf53507457f023c2ef6aaa01116b091f12c480
jimmy <773792867@qq.com>
2018-09-18 09:10:48 +0800

    elasticsearch

--- tutorial_lombok/src/test/test/com/lombok/test/LombokDemo.java
+++ tutorial_lombok/src/test/test/com/lombok/test/LombokDemo.java
@@ -29,9 +29,9 @@ public class LombokDemo {
     }
     /**
      * @Accessors(chain = true)
-     * fluent boolean值，默认为false。此字段主要为控制生成的getter和setter方法前面是否带get/set
-     * chain boolean值，默认false。如果设置为true，setter返回的是此对象，方便链式调用方法
-     * prefix 设置前缀 例如：@Accessors(prefix = "abc") private String abcAge 当生成get/set方法时，会把此前缀去掉
+     * fluent boolean值,默认为false.此字段主要为控制生成的getter和setter方法前面是否带get/set
+     * chain boolean值,默认false.如果设置为true,setter返回的是此对象,方便链式调用方法
+     * prefix 设置前缀 例如:@Accessors(prefix = "abc") private String abcAge 当生成get/set方法时,会把此前缀去掉
      */
     @Test
     public void accessorsTest(){
