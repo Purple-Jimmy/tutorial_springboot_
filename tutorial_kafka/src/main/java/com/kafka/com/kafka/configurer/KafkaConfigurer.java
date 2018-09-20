3783f921b8c4f27a9dcd24cb593d1d604374937c
jimmy <773792867@qq.com>
2018-09-20 17:36:42 +0800

    stream

--- tutorial_kafka/src/main/java/com/kafka/com/kafka/configurer/KafkaConfigurer.java
+++ tutorial_kafka/src/main/java/com/kafka/com/kafka/configurer/KafkaConfigurer.java
@@ -13,7 +13,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Created by jimmy on 2017/7/24.
+ * @Author: jimmy
+ * @Date: 2018/9/14
  */
 @Configuration
 @EnableKafka
@@ -21,7 +22,7 @@ public class KafkaConfigurer {
 
     public Map<String, Object> producerConfigs() {
         Map<String, Object> props = new HashMap<String, Object>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"192.168.50.213:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"192.168.50.244:9092");
         props.put(ProducerConfig.RETRIES_CONFIG, 0);
         props.put(ProducerConfig.BATCH_SIZE_CONFIG, 4096);
         props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
