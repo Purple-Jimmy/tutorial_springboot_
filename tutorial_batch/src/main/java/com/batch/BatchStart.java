7137e6f594dbff583ff99fdbc639a80ca40a1b31
zhoucheng <574693497@qq.com>
2018-11-11 23:53:52 +0800

    java set

---
+++ tutorial_batch/src/main/java/com/batch/BatchStart.java
@@ -0,0 +1,16 @@
+package com.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jimmy
+ * @date 2018/11/1122:43
+ */
+@SpringBootApplication
+public class BatchStart {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchStart.class,args);
+    }
+}
