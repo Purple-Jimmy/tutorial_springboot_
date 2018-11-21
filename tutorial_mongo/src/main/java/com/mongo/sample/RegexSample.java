93f8724897b22d97d43a13ea9f09b1170cbc8b8b
jimmy <773792867@qq.com>
2018-11-21 11:11:12 +0800

    spring boot

--- tutorial_mongo/src/main/java/com/mongo/sample/RegexSample.java
+++ tutorial_mongo/src/main/java/com/mongo/sample/RegexSample.java
@@ -16,4 +16,36 @@ public class RegexSample {
             criteriaTotal.orOperator(cr);
         }
     }*/
+
+
+    /*Query query = new Query();
+    //过滤掉没有挂栏目且自动入栏的节目集
+        query.addCriteria(Criteria.where("categoryId").ne(null).and("isTvsouAuto").ne(1));
+
+
+    BasicDBObject obj= BasicDBObject.parse("{categoryId: {$ne:null}, isTvsouAuto: {$ne:1}}");
+
+    DistinctIterable<Long> distinct = mongoTemplate.getCollection("epgGroupPs").distinct("programsetId",obj,Long.class);
+    MongoCursor<Long> iterator = distinct.iterator();
+
+    List<Long> myList = new ArrayList<>();
+        while(iterator.hasNext()){
+        myList.add(iterator.next());
+    }
+        System.out.println(myList.size());
+
+
+    Aggregation agg = Aggregation.newAggregation(
+            Aggregation.match(Criteria.where("categoryId").ne(null).and("isTvsouAuto").ne(1)),  //查询条件
+            Aggregation.group("programsetId"),
+            // Aggregation.sort(sort),
+            Aggregation.skip(0),//跳到第几个开始
+            Aggregation.limit(10)//查出多少个数据
+    );
+
+    AggregationResults<BasicDBObject> outputType=mongoTemplate.aggregate(agg,"epgGroupPs", BasicDBObject.class);
+        System.out.println(outputType);
+*/
+
+
 }
