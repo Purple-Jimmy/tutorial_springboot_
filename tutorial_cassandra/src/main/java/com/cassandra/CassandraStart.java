83a7fd8bde29f71cffc5520543e8b1034eed8253
jimmy <773792867@qq.com>
2018-12-05 00:02:47 +0800

    spring boot

---
+++ tutorial_cassandra/src/main/java/com/cassandra/CassandraStart.java
@@ -0,0 +1,15 @@
+package com.cassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: jimmy
+ * @Date: 2018/12/5
+ */
+@SpringBootApplication
+public class CassandraStart {
+    public static void main(String[] args) {
+        SpringApplication.run(CassandraStart.class,args);
+    }
+}
