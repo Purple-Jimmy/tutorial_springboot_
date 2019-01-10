4dc6d4245f0952ff27d9f52a510f0a0246c9ef0e
jimmy <773792867@qq.com>
2019-01-10 15:49:01 +0800

    hutool

--- tutorial_java/src/main/java/com/java/lambda/demo/MathOperateInterface.java
+++ tutorial_java/src/main/java/com/java/lambda/demo/MathOperateInterface.java
@@ -4,7 +4,13 @@ package com.java.lambda.demo;
  * @Author: jimmy
  * @Date: 2018/8/9
  */
+@FunctionalInterface
 public interface MathOperateInterface {
-
+    /**
+     * 操作
+     * @param a
+     * @param b
+     * @return
+     */
     int operate(int a,int b);
 }
