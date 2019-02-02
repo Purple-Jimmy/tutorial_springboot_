48443490346488699eb27d47442e91cd028c352d
zhoucheng <574693497@qq.com>
2019-02-02 21:55:10 +0800

    java base

---
+++ tutorial_java/src/main/java/com/java/domain/City.java
@@ -0,0 +1,28 @@
+package com.java.domain;
+
+/**
+ * @author jimmy
+ * @date 2019-02-0218:19
+ */
+public class City {
+
+    private Long id;
+    private String name;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
