bd8d80dbd4c10a5037885784a03598607e6325de
jimmy <773792867@qq.com>
2019-02-26 10:47:40 +0800

    java

--- tutorial_java/src/main/java/com/java/collection/demo/ListDemo.java
+++ tutorial_java/src/main/java/com/java/collection/demo/ListDemo.java
@@ -10,12 +10,22 @@ import java.util.List;
 public class ListDemo {
 
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
+        List<String> list = new ArrayList<>();
         for(int i=1;i<=10;i++){
-            list.add(i);
+            list.add(i+"");
+        }
+        list.add("6");
+        System.out.println(list);
+
+        for(String i:list){
+            if(i.equalsIgnoreCase("6")){
+                list.remove(i);
+                break;
+            }
         }
         System.out.println(list);
+       /* System.out.println(list);
         System.out.println(list.subList(0,3));
-        System.out.println(list.subList(list.size()-3,list.size()));
+        System.out.println(list.subList(list.size()-3,list.size()));*/
     }
 }
