4ed2aae944d973d8da7d949d9d1188cf067e0c3d
zhoucheng <574693497@qq.com>
2019-03-13 22:38:40 +0800

    java

--- tutorial_java/src/test/java/com/reflect/demo/ReflectBasicDemo.java
+++ tutorial_java/src/test/java/com/reflect/demo/ReflectBasicDemo.java
@@ -3,14 +3,39 @@ package com.reflect.demo;
 import com.java.domain.Car;
 import org.junit.Test;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
+import java.lang.reflect.*;
 
 /**
  * @author jimmy
  * @date 2019-03-1222:29
  */
 public class ReflectBasicDemo {
+
+    /**
+     * Class有一个newInstance()方法可以创建对象,会调用默认的无参public构造方法
+     * 如果没有此构造方法会抛出异常
+     */
+    @Test
+    public void classTest() throws IllegalAccessException, InstantiationException {
+        Car car = Car.class.newInstance();
+        System.out.println(car);
+        //获取所有的public构造方法
+        Constructor<?>[] constructors = Car.class.getConstructors();
+        System.out.println(constructors);
+        //获取所有的构造方法,包括非public的
+        Constructor<?>[] allConstructor= Car.class.getDeclaredConstructors();
+        System.out.println(allConstructor);
+    }
+
+    /**
+     * Constructor类表示构造方法
+     * constructor.newInstance()可以创建对象
+     */
+    @Test
+    public void constructorTest() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        Constructor<StringBuilder> constructor = StringBuilder.class.getConstructor(new Class[]{int.class});
+        System.out.println(constructor.newInstance(100));
+    }
 
     /**
      * 获取名称信息
@@ -65,8 +90,6 @@ public class ReflectBasicDemo {
      * isFinal: false
      * isVolatile: false
      * class java.lang.String
-     * @throws NoSuchFieldException
-     * @throws IllegalAccessException
      */
     @Test
     public void fieldInfoTest2() throws NoSuchFieldException, IllegalAccessException {
@@ -98,9 +121,42 @@ public class ReflectBasicDemo {
         System.out.println(field.getType());
     }
 
+    /**
+     * [Ljava.lang.reflect.Method;@3a4afd8d
+     * [Ljava.lang.reflect.Method;@1996cd68
+     * public java.lang.String com.java.domain.Car.getBrand()
+     * public void com.java.domain.Car.setBrand(java.lang.String)
+     * public java.lang.Long com.java.domain.Car.getPrice()
+     */
     @Test
-    public void methodInfoTest(){
+    public void methodInfoTest1() throws ClassNotFoundException, NoSuchMethodException {
+        Class clazz = Class.forName("com.java.domain.Car");
+        //获取所有的public方法,包括父类的
+        System.out.println(clazz.getMethods());
+        //获取本类的所有方法,包括私有的,但不包括父类的
+        System.out.println(clazz.getDeclaredMethods());
+        //返回本类或父类中指定名称和参数类型的public方法
+        System.out.println(clazz.getMethod("getBrand"));
+        System.out.println(clazz.getMethod("setBrand",String.class));
+        //返回本类中声明的指定名称和参数类型的方法
+        System.out.println(clazz.getMethod("getPrice"));
+    }
 
+    /**
+     * getBrand
+     * Lexus
+     */
+    @Test
+    public void methodInfoTest2() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Car car = new Car();
+        car.setBrand("Lexus");
+        Method method = car.getClass().getMethod("getBrand");
+        //获取方法的名称
+        System.out.println(method.getName());
+        //true:忽略java的访问检查机制,允许读写非public的方法
+        method.setAccessible(true);
+        //在指定对象obj上调用Method代表的方法,参数列表为args
+        System.out.println(method.invoke(car));
     }
 
 }
