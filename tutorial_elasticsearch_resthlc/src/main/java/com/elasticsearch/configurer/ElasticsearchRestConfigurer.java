379c9589b42f32c43b050634ea51f9d9a7b3d579
jimmy <773792867@qq.com>
2019-03-21 17:15:06 +0800

    es

---
+++ tutorial_elasticsearch_resthlc/src/main/java/com/elasticsearch/configurer/ElasticsearchRestConfigurer.java
@@ -0,0 +1,113 @@
+package com.elasticsearch.configurer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+/**
+ * @Author: jimmy
+ * @Date: 2019/3/21
+ */
+@Slf4j
+@Configuration
+public class ElasticsearchRestConfigurer {
+    @Value("${elasticsearch.urls}")
+    String[] urls;
+
+    @Value("${elasticsearch.username}")
+    private String userName;
+
+    @Value("${elasticsearch.password}")
+    private String password;
+
+    /**
+     * 连接超时时间
+     */
+    private static int connectTimeOut = 1000;
+    /**
+     * 连接超时时间
+     */
+    private static int socketTimeOut = 30000;
+    /**
+     *  获取连接的超时时间
+     */
+    private static int connectionRequestTimeOut = 500;
+    /**
+     * 最大连接数
+     */
+    private static int maxConnectNum = 100;
+    /**
+     * 最大路由连接数
+     */
+    private static int maxConnectPerRoute = 100;
+
+
+
+    private static final int ADDRESS_LENGTH = 2;
+    private static final String HTTP_SCHEME = "http";
+
+    @Bean
+    public RestClientBuilder restClientBuilder() {
+        HttpHost[] hosts = Arrays.stream(urls)
+                .map(this::makeHttpHost)
+                .filter(Objects::nonNull)
+                .toArray(HttpHost[]::new);
+        log.info("hosts:{}", Arrays.toString(hosts));
+        return RestClient.builder(hosts);
+    }
+
+
+    @Bean(name = "highLevelClient")
+    public RestHighLevelClient highLevelClient(@Autowired RestClientBuilder restClientBuilder) {
+        return new RestHighLevelClient(restClientBuilder);
+    }
+
+   /* @Bean
+    public RestHighLevelClient client() {
+        RestClientBuilder builder = RestClient.builder(hostList.toArray(new HttpHost[0]));
+        // 异步httpclient连接延时配置
+        builder.setRequestConfigCallback(new RestClientBuilder.RequestConfigCallback() {
+            @Override
+            public Builder customizeRequestConfig(Builder requestConfigBuilder) {
+                requestConfigBuilder.setConnectTimeout(connectTimeOut);
+                requestConfigBuilder.setSocketTimeout(socketTimeOut);
+                requestConfigBuilder.setConnectionRequestTimeout(connectionRequestTimeOut);
+                return requestConfigBuilder;
+            }
+        });
+        // 异步httpclient连接数配置
+        builder.setHttpClientConfigCallback(new HttpClientConfigCallback() {
+            @Override
+            public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+                httpClientBuilder.setMaxConnTotal(maxConnectNum);
+                httpClientBuilder.setMaxConnPerRoute(maxConnectPerRoute);
+                return httpClientBuilder;
+            }
+        });
+        RestHighLevelClient client = new RestHighLevelClient(builder);
+        return client;
+    }
+*/
+
+    private HttpHost makeHttpHost(String s) {
+        assert StringUtils.isNotEmpty(s);
+        String[] address = s.split(":");
+        if (address.length == ADDRESS_LENGTH) {
+            String ip = address[0];
+            int port = Integer.parseInt(address[1]);
+            return new HttpHost(ip, port, HTTP_SCHEME);
+        } else {
+            return null;
+        }
+    }
+}
