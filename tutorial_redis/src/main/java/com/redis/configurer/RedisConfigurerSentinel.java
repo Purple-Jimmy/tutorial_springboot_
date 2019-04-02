c698acacf2187263b1811c7237c2e92417053076
jimmy <773792867@qq.com>
2019-04-02 10:18:24 +0800

    redis

---
+++ tutorial_redis/src/main/java/com/redis/configurer/RedisConfigurerSentinel.java
@@ -0,0 +1,228 @@
+package com.redis.configurer;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.cache.Cache;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.interceptor.CacheErrorHandler;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.connection.RedisNode;
+import org.springframework.data.redis.connection.RedisSentinelConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @Author: jimmy
+ * @Date: 2018/8/13
+ */
+@Configuration
+@AutoConfigureAfter(RedisAutoConfiguration.class)
+@Slf4j
+public class RedisConfigurerSentinel extends CachingConfigurerSupport {
+    private static final Logger logger = LoggerFactory.getLogger(RedisConfigurerSentinel.class);
+    @Autowired
+    private Environment environment;
+
+    @Value("${spring.redis.lettuce.pool.max-active}")
+    private Integer maxActive;
+    @Value("${spring.redis.lettuce.pool.max-idle}")
+    private Integer maxIdle;
+    @Value("${spring.redis.lettuce.pool.min-idle}")
+    private Integer minIdle;
+
+    @Bean
+    LettuceConnectionFactory lettuceConnectionFactory() {
+        // 连接池配置
+        GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
+        poolConfig.setMaxIdle(maxIdle == null ? 8 : maxIdle);
+        poolConfig.setMinIdle(minIdle == null ? 1 : minIdle);
+       // poolConfig.setMaxTotal(maxTotal == null ? 8 : maxTotal);
+        poolConfig.setMaxWaitMillis(5000L);
+        LettucePoolingClientConfiguration lettucePoolingClientConfiguration = LettucePoolingClientConfiguration.builder()
+                .poolConfig(poolConfig)
+                .build();
+        // 单机redis
+      /*  RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+        redisConfig.setHostName(host==null||"".equals(host)?"localhost":host.split(":")[0]);
+        redisConfig.setPort(Integer.valueOf(host==null||"".equals(host)?"6379":host.split(":")[1]));
+        if (password != null && !"".equals(password)) {
+            redisConfig.setPassword(password);
+        }*/
+
+        Set<RedisNode> nodes = new HashSet<>();
+        String[] hosts = environment.getProperty("spring.redis.cluster.nodes").split(",");
+        for (String h : hosts) {
+            h = h.replaceAll("\\s", "").replaceAll("\n", "");
+            if (!"".equals(h)) {
+                String host = h.split(":")[0];
+                int port = Integer.valueOf(h.split(":")[1]);
+                nodes.add(new RedisNode(host, port));
+            }
+        }
+
+        // 哨兵redis
+         RedisSentinelConfiguration redisConfig = new RedisSentinelConfiguration();
+         redisConfig.setMaster("mymaster");
+         redisConfig.setSentinels(nodes);
+
+        // 集群redis
+        //RedisClusterConfiguration redisConfig = new RedisClusterConfiguration();
+        //redisConfig.setClusterNodes(nodes);
+        // 跨集群执行命令时要遵循的最大重定向数量
+        //redisConfig.setMaxRedirects(3);
+       // redisConfig.setPassword(password);
+
+        return new LettuceConnectionFactory(redisConfig, lettucePoolingClientConfiguration);
+    }
+
+
+
+    /**
+     * StringRedisTemplate继承自RedisTemplate,只能操作键值都是String类型的数据
+     * @return
+     */
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(@Autowired LettuceConnectionFactory lettuceConnectionFactory) {
+        logger.info("StringRedisTemplate 初始化...");
+        StringRedisTemplate redisTemplate = new StringRedisTemplate();
+        redisTemplate.setConnectionFactory(lettuceConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        //redisTemplate.setValueSerializer(new RedisObjectSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+
+    /**
+     * 对象转成json格式
+     * @return
+     */
+    @Bean
+    public RedisTemplate<String,Object> redisTemplate(@Autowired LettuceConnectionFactory lettuceConnectionFactory){
+        logger.info("redisTemplate 初始化...");
+        RedisTemplate<String,Object> redisTemplate = new RedisTemplate<String, Object>();
+        redisTemplate.setConnectionFactory(lettuceConnectionFactory);
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        objectMapper.configure(MapperFeature.USE_ANNOTATIONS, false);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
+
+    @Bean
+    public RedisCacheConfiguration redisCacheConfiguration() {
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        objectMapper.configure(MapperFeature.USE_ANNOTATIONS, false);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig();
+        configuration = configuration
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer));
+                //.entryTtl(Duration.ofSeconds(20));
+        return configuration;
+    }
+
+    @Override
+    @Bean
+    public KeyGenerator keyGenerator() {
+        return (target, method, params) -> {
+            StringBuilder sb = new StringBuilder();
+            sb.append(target.getClass().getName());
+            sb.append(method.getName());
+            for (Object obj : params) {
+                sb.append(obj.toString());
+            }
+            return sb.toString();
+        };
+    }
+
+
+
+
+    @Override
+    @Bean
+    public CacheErrorHandler errorHandler() {
+        CacheErrorHandler cacheErrorHandler = new CacheErrorHandler() {
+            @Override
+            public void handleCacheGetError(RuntimeException e, Cache cache, Object key) {
+                log.error("Method:errorHandler Redis occur handleCacheGetError：key -> [{}]", key, e);
+            }
+
+            @Override
+            public void handleCachePutError(RuntimeException e, Cache cache, Object key, Object value) {
+                log.error("Method:errorHandler Redis occur handleCachePutError：key -> [{}]；value -> [{}]", key, value, e);
+            }
+
+            @Override
+            public void handleCacheEvictError(RuntimeException e, Cache cache, Object key) {
+                log.error("Method:errorHandler Redis occur handleCacheEvictError：key -> [{}]", key, e);
+            }
+
+            @Override
+            public void handleCacheClearError(RuntimeException e, Cache cache) {
+                log.error("Method:errorHandler Redis occur handleCacheClearError：", e);
+            }
+        };
+        return cacheErrorHandler;
+    }
+
+}
+
+
+
+
+//private String host;
+//private String port;
+
+
+   /* @Bean
+    public LettuceConnectionFactory redisStringConnectionFactory(){
+        LettuceConnectionFactory factory = new LettuceConnectionFactory();
+        factory.setDatabase(10);
+        factory.set
+        return factory;
+    }*/
+
